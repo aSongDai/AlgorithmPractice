aee63be6d2b4d863591a5d8c9e4791316ec86367
aSong <484932151@qq.com>
2024-02-28 14:21:35 +0800

    solvesudoku

---
+++ cs_view/code/SolveSudoku.cpp
@@ -0,0 +1,51 @@
+#include <vector>	
+
+class Solution
+{
+public:
+	void solveSudoku(std::vector<std::vector<char>>& board)
+	{
+		std::memset(line, false, sizeof line);
+		std::memset(column, false, sizeof column);
+		std::memset(block, false, sizeof block);
+		valid = false;
+
+		for (int i = 0; i < 9; ++i)
+		{
+			for (int j = 0; j < 9; ++j)
+			{
+				if (board[i][j] == '.')
+				{
+					// 
+					spaces.push_back({i, j});
+				}
+				else
+				{
+					int digit = board[i][j] - '0';
+					line[i][digit - 1] = column[digit - 1][j] = block[i / 3][j / 3][digit - 1] = true;
+				}
+			}
+		}
+
+		dfs(board, 0);
+	}
+
+	void dfs(std::vector<std::vector<char>>& board, int pos)
+	{
+		if (pos == spaces.size())
+		{
+			valid = true;
+			return;
+		}
+
+		auto [i, j] = spaces[pos];					// struct bind (c++17)
+
+	}
+
+private:
+	bool line[9][9];
+	bool column[9][9];
+	bool block[3][3][9];
+	bool valid;
+	std::vector<std::pair<int, int>> spaces;		// ¼ÇÂ¼ÔÚÄÄ¶ù»ØËÝ
+};
